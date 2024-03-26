1d35e2221f7264dc24776db2651844f26b3f346a
Benjamin Mynhier <benjmyn@vt.edu>
2024-03-26 13:03:42 -0400

    Add files via upload
    Add acceleration track; 
    Updated quasi-static acceleration functions;
    Started logging Gx, Gy, plot GGV

--- VTMPDLS_v09.m
+++ VTMPDLS_v09.m
@@ -29,20 +29,23 @@ Car(1) = CarPM('c1.json',World);
 
 %% Utilize GGV
 figure
-load("skidpad.mat")
+load("Zandvoort.mat")
+% SWP = linspace(0,100,100);
+% KWP = [100,linspace(0.04,0.04,48),0.1,linspace(0.06,0.06,50)];
+
 RWP = abs(1./KWP);
 % RWP(1) = 0.01; % has to be > 0
 RWP(~isfinite(RWP)) = 10000;
 
 VWP = cornVel(Car(1),SWP,RWP);
-plot(SWP,VWP,'k>')
+plot(SWP,VWP,'k+')
 xlim([-1,SWP(end)+1])
 ylim([0,40])
 hold on
 
 
 disp("Walking points...")
-MVWP = walk(Car(1),SWP,VWP);
+[MVWP,GX,GY] = walk(Car(1),SWP,VWP,RWP);
 disp("Done walking points")
 
 plot(SWP,MVWP,'g.-')
@@ -51,13 +54,18 @@ fprintf("Car 1 traversed track in %3.3f seconds.\n",Car(1).laptime)
 xlabel("Distance (m)")
 ylabel("Velocity (m/s)")
 
+figure
+plot3(GY,GX,MVWP,'.')
+
 
 %% Functions
-function MVWP = walk(Car,SWP,VWP)
+function [MVWP,GX,GY] = walk(Car,SWP,VWP,RWP)
     % Initialize velocity vectors
     UWP = SWP.*0; 
+    a = SWP.*0;
     MVWP = VWP.*0 + realmax;
-    
+    GX = MVWP;
+    GY = MVWP;
     for n = 1:length(SWP)
         % Set current waypoint velocity to max cornering velocity
         UWP(n) = VWP(n); 
@@ -65,15 +73,20 @@ function MVWP = walk(Car,SWP,VWP)
             % Walk forward from n
             for i = (n+1):length(SWP)
                 dx = SWP(i) - SWP(i-1); % cur-last
-                UWP(i) = sqrt(UWP(i-1).^2 + 0.5 .* Car.axT_fcn(UWP(i-1)) .* dx);
+                a(i) = real(sqrt(Car.axT_fcn(UWP(i-1)).^2 - (Car.ax_scale.*UWP(i-1).^2./RWP(i-1)).^2));
+                UWP(i) = sqrt(UWP(i-1).^2 + 2.*a(i).*dx);
             end
             % Walk backward from n
             for i = 1:(n-1)
                 j = n-i; % reverse i
                 dx = SWP(j+1) - SWP(j); % next-cur
-                UWP(j) = sqrt(UWP(j+1).^2 + 0.5 .* Car.axB_fcn(UWP(j+1)) .* dx);
+                a(j) = -real(sqrt(Car.axB_fcn(UWP(j+1)).^2 - (Car.ax_scale.*UWP(j+1).^2./RWP(j+1)).^2));
+                UWP(j) = sqrt(UWP(j+1).^2 - 2.*a(j).*dx);
             end
-            MVWP = MVWP .* (MVWP <= UWP) + UWP .* (MVWP > UWP); 
+
+            GX =   GX  .* (MVWP <= UWP)        +      a./9.8 .* (MVWP > UWP);
+            GY =   GY  .* (MVWP <= UWP) + (UWP.^2./RWP)./9.8 .* (MVWP > UWP);
+            MVWP = MVWP .* (MVWP <= UWP)       +         UWP .* (MVWP > UWP); 
             
             % Display point progress (optional)
             fprintf(n+"/"+length(SWP)+"\n")
@@ -83,6 +96,7 @@ function MVWP = walk(Car,SWP,VWP)
 end
 
 function VWP = cornVel(Car,SWP,RWP)
+    disp("Calculating cornering velocities")
     VWP = SWP.*0;
     for i = 1:length(SWP)
         err = @(v) v.*abs(v) ./ RWP(i) - Car.ay_fcn(v);
