7b9968d99ccf9f04655ecfbab2760395946ea355
Benjamin Mynhier <benjmyn@vt.edu>
2024-03-27 00:24:33 -0400

    Add files via upload
    Added powertrain functionality

--- CarPM.m
+++ CarPM.m
@@ -18,7 +18,12 @@ classdef CarPM
         ax_scale(1,1);
         rpm;
         torque;
-        gearing;
+        gear_ratio;
+        trans_ratio;
+        shift_vel;
+        primary_drive(1,1);
+        final_drive(1,1);
+        wheel_radius(1,1);
         
         % Functions
         pajecka_fcn;
@@ -36,21 +41,35 @@ classdef CarPM
         ay_fcn;
 
         % Classes/Structs
-        
+        Log;
+
     end
     methods
-        function [Car] = CarPM(json,World)
+        function [Car] = CarPM(car_json,powertrain_json,World)
             % Read .json to Struct
-            Str = readstruct(json);
-            
-            % Convert Struct to Object
-            for fn = fieldnames(Str)' 
+            CJ = readstruct(car_json);
+            PJ = readstruct(powertrain_json);
+
+            % Upload Struct Data to Object
+            for fn = fieldnames(CJ)' 
                 try 
-                    Car.(fn{1}) = Str.(fn{1});
+                    Car.(fn{1}) = CJ.(fn{1});
                 catch
-                    warning("Unable to assign field """+fn{1}+"""");
+                    warning("Unable to assign car field """+fn{1}+"""");
                 end
             end
+            for fn = fieldnames(PJ)' 
+                try 
+                    Car.(fn{1}) = PJ.(fn{1});
+                catch
+                    warning("Unable to assign powertrain field """+fn{1}+"""");
+                end
+            end
+
+            % Resolve leftover properties
+            Car.W = Car.m .* 9.8;
+            Car.trans_ratio = Car.primary_drive .* Car.final_drive .* Car.gear_ratio;
+            Car.shift_vel = Car.rpm(end) .* (2*pi/60) ./ Car.trans_ratio .* Car.wheel_radius;
 
             % Define Pajecka Function
             Car.pajecka_fcn = @(N) World.grip_scale .* (Car.B4(1) - Car.B4(2).*N) .* N ...
@@ -61,16 +80,35 @@ classdef CarPM
             Car.lift_fcn = @(v) 1/2 .* Car.Cl .* Car.A .* World.p .* v.^2; 
 
             % Define driving forces
-            Car.W = Car.m .* 9.8;
             Car.N_fcn = @(v) (Car.W + Car.lift_fcn(v));
             Car.FxB_fcn = @(v) 4 .* Car.ax_scale .* Car.pajecka_fcn(Car.N_fcn(v) ./ 4) + Car.drag_fcn(v);
-            Car.FxT_fcn = @(v) 4 .* Car.ax_scale .* Car.pajecka_fcn(Car.N_fcn(v) ./ 4) - Car.drag_fcn(v);
+            Car.FxT_fcn = @(v) min(4 .* Car.ax_scale .* Car.pajecka_fcn(Car.N_fcn(v) ./ 4),...
+                                   Car.powertrainLookup(v)) - Car.drag_fcn(v);
             Car.Fy_fcn = @(v) 4 .* Car.pajecka_fcn(Car.N_fcn(v) ./ 4);
 
             % Define GGV accelerations (in g's)
             Car.axB_fcn = @(v) Car.FxB_fcn(v) ./ Car.m;
             Car.axT_fcn = @(v) Car.FxT_fcn(v) ./ Car.m;
             Car.ay_fcn = @(v) Car.Fy_fcn(v) ./ Car.m;
+        end
+        function [Fx,cur_gear,rpm] = powertrainLookup(Car,v)
+            Fx = linspace(0,0,length(v));
+            cur_gear = linspace(0,0,length(v));
+            rpm = linspace(0,0,length(v));
+            torque = linspace(0,0,length(v));
+            for vi = 1:length(v)
+                if v(vi) >= Car.shift_vel(end)
+                    cur_gear(vi) = length(Car.gear_ratio);
+                    rpm(vi) = Car.rpm(end);
+                    % torque = Car.torque(end) .* Car.trans_ratio()
+                    Fx(vi) = Car.drag_fcn(v(vi));
+                else
+                    cur_gear(vi) = interp1([0,Car.shift_vel],0:length(Car.gear_ratio),v(vi),"next");
+                    rpm(vi) = v(vi) ./ (Car.wheel_radius) .* (2*pi/60)^-1 .* Car.trans_ratio(cur_gear(vi));
+                    torque(vi) = interp1(Car.rpm,Car.torque,rpm(vi),'linear') .* Car.trans_ratio(cur_gear(vi));
+                    Fx(vi) = torque(vi) ./ Car.wheel_radius;
+                end
+            end
         end
     end
 end
--- VTMPDLS_v09.m
+++ VTMPDLS_v09.m
@@ -21,7 +21,7 @@ clc, clear, close all
 World.p = 1.225; % air density
 World.grip_scale = 0.80; % tire grip factor vs. TTC data
 
-Car(1) = CarPM('c1.json',World);
+Car(1) = CarPM('c1.json','p1.json',World);
 
 %% Plot GGV
 % v = 0:30;
@@ -29,7 +29,7 @@ Car(1) = CarPM('c1.json',World);
 
 %% Utilize GGV
 figure
-load("Zandvoort.mat")
+load("accel.mat")
 % SWP = linspace(0,100,100);
 % KWP = [100,linspace(0.04,0.04,48),0.1,linspace(0.06,0.06,50)];
 
@@ -45,54 +45,58 @@ hold on
 
 
 disp("Walking points...")
-[MVWP,GX,GY] = walk(Car(1),SWP,VWP,RWP);
+Log = walk(Car(1),SWP,VWP,RWP);
 disp("Done walking points")
 
-plot(SWP,MVWP,'g.-')
-Car(1).laptime = lapTime(SWP,MVWP);
+plot(SWP,Log.MVWP,'g.-')
+Car(1).laptime = lapTime(SWP,Log.MVWP);
 fprintf("Car 1 traversed track in %3.3f seconds.\n",Car(1).laptime)
 xlabel("Distance (m)")
 ylabel("Velocity (m/s)")
 
-figure
-plot3(GY,GX,MVWP,'.')
+% figure
+% plot3(GY,GX,MVWP,'.')
 
 
 %% Functions
-function [MVWP,GX,GY] = walk(Car,SWP,VWP,RWP)
+function Log = walk(Car,SWP,VWP,RWP)
     % Initialize velocity vectors
     UWP = SWP.*0; 
     a = SWP.*0;
-    MVWP = VWP.*0 + realmax;
-    GX = MVWP;
-    GY = MVWP;
+    Log.MVWP = VWP.*0 + realmax;
+    Log.GX = Log.MVWP;
+    Log.GY = Log.MVWP;
+    Log.gear = Log.MVWP.*0;
+    Log.RPM = Log.MVWP.*0;
+    
     for n = 1:length(SWP)
         % Set current waypoint velocity to max cornering velocity
         UWP(n) = VWP(n); 
-        if VWP(n) < MVWP(n)
+        if VWP(n) < Log.MVWP(n)
             % Walk forward from n
             for i = (n+1):length(SWP)
                 dx = SWP(i) - SWP(i-1); % cur-last
-                a(i) = real(sqrt(Car.axT_fcn(UWP(i-1)).^2 - (Car.ax_scale.*UWP(i-1).^2./RWP(i-1)).^2));
+                a(i) = real(sqrt(Car.axT_fcn(UWP(i-1)).^2 - (Car.ax_scale.*UWP(i-1).^2./RWP(i)).^2));
                 UWP(i) = sqrt(UWP(i-1).^2 + 2.*a(i).*dx);
             end
             % Walk backward from n
             for i = 1:(n-1)
                 j = n-i; % reverse i
                 dx = SWP(j+1) - SWP(j); % next-cur
-                a(j) = -real(sqrt(Car.axB_fcn(UWP(j+1)).^2 - (Car.ax_scale.*UWP(j+1).^2./RWP(j+1)).^2));
+                a(j) = -real(sqrt(Car.axB_fcn(UWP(j+1)).^2 - (Car.ax_scale.*UWP(j+1).^2./RWP(j)).^2));
                 UWP(j) = sqrt(UWP(j+1).^2 - 2.*a(j).*dx);
             end
 
-            GX =   GX  .* (MVWP <= UWP)        +      a./9.8 .* (MVWP > UWP);
-            GY =   GY  .* (MVWP <= UWP) + (UWP.^2./RWP)./9.8 .* (MVWP > UWP);
-            MVWP = MVWP .* (MVWP <= UWP)       +         UWP .* (MVWP > UWP); 
+            Log.GX =   Log.GX  .* (Log.MVWP <= UWP)        +      a./9.8 .* (Log.MVWP > UWP);
+            Log.GY =   Log.GY  .* (Log.MVWP <= UWP) + (UWP.^2./RWP)./9.8 .* (Log.MVWP > UWP);
+            Log.MVWP = Log.MVWP .* (Log.MVWP <= UWP)       +         UWP .* (Log.MVWP > UWP);
+            
             
             % Display point progress (optional)
             fprintf(n+"/"+length(SWP)+"\n")
         end
-        
     end
+    [~,Log.gear,Log.RPM] = Car.powertrainLookup(Log.MVWP);
 end
 
 function VWP = cornVel(Car,SWP,RWP)
