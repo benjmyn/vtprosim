76820f0b1ee766df27a1a0e5e9f1485ad7f32641
Benjamin Mynhier <benjmyn@vt.edu>
2024-03-30 21:44:54 -0400

    Add files via upload
    Redid Car4W but currently janky brake model

--- Car4W.m
+++ Car4W.m
@@ -1,36 +1,41 @@
 
 classdef Car4W
     properties
-        % Scalar Properties
+        % Vehicle Functions
+        pajecka_fcn;
+        drag_fcn;
+        lift_fcn;
+        axB_fcn;
+        axT_fcn;
+        ay_fcn;
+        L_fcn;
+        motor_fcn;
+        % General Vehicle Properties
         m(1,1);
-            % Car mass (with driver)
         W(1,4);
-            % (1) = FL
-            % (2) = FR 
-            % (3) = RL
-            % (4) = RR
+        % Tire Properties
+        B4(1,4);
+        ax_scale(1,1);
+        % Suspension Properties
         a(1,1);
         b(1,1);
-            % a, b = Lengths from F/R axle to CG
+        l(1,1);
+        h(1,1);
         tf(1,1);
         tr(1,1);
-            % tf, tr = F/R track
-        h(1,1);
-        zf(1,1);
-        zr(1,1);
-            %zf, zr = F/R roll center height
         KF(1,1);
         KR(1,1);
-            % kf, kr = F/R roll stiffness
-        B4(1,4);
-            % (1) = Max G's
-            % (2) = G's lost per N
-            % (3) = ???
-            % (4) = ???
-        Cd(1,1);
+        zf(1,1);
+        zr(1,1);
+        % Aerodynamic Properties
         Cl(1,1);
+        Cd(1,1);
         A(1,1);
-        ax_scale(1,1);
+        p(1,1);
+        CoPa(1,1);
+        CoPb(1,1);
+        CoPh(1,1);
+        % Powertrain Properties
         rpm;
         torque;
         gear_ratio;
@@ -38,99 +43,94 @@ classdef Car4W
         shift_vel;
         primary_drive(1,1);
         final_drive(1,1);
-        wheel_radius(1,4);
-
-        % Functions
-        pajecka_fcn;
-        motor_fcn;
-        drag_fcn;
-        lift_fcn;
-        N_fcn;
-        FxB_fcn;
-        FxT_fcn;
-        Fy_fcn;
-        
+        wheel_radius(1,1);
     end
     methods
-        function [Car] = Car4W(car_json,powertrain_json,World)
-            % Constructor Function
-            % Read .json to Struct
-            CJ = readstruct(car_json);
-            PJ = readstruct(powertrain_json);
-
-            % Upload Struct Data to Object
+        function Car = Car4W(carJson,powerJson,World)
+            % Load properties from JSON files
+            CJ = readstruct(carJson);
+            PJ = readstruct(powerJson);
             for fn = fieldnames(CJ)' 
                 try 
                     Car.(fn{1}) = CJ.(fn{1});
                 catch
-                    warning("Unable to assign car field """+fn{1}+"""");
+                    % warning("Unable to assign car field """+fn{1}+"""");
                 end
             end
             for fn = fieldnames(PJ)' 
                 try 
                     Car.(fn{1}) = PJ.(fn{1});
                 catch
-                    warning("Unable to assign powertrain field """+fn{1}+"""");
+                    % warning("Unable to assign powertrain field """+fn{1}+"""");
                 end
             end
-            Car.W = Car.transferWeight(0,0);
 
-            % Define Pajecka Function
-            Car.pajecka_fcn = @(N) World.grip_scale .* (Car.B4(1) - Car.B4(2).*N) .* N ...
-                                   .*heaviside(Car.B4(1) - Car.B4(2).*N); % if < 0, = 0
-
+            % Fill in derived properties
+            Car.l = Car.a + Car.b;
+            Car.W = Car.m .* 4.9 .* [Car.b,Car.b,Car.a,Car.a] ./ (Car.l);
+            
             % Define aerodynamic forces
             Car.drag_fcn = @(v) 1/2 .* Car.Cd .* Car.A .* World.p .* v.^2;
             Car.lift_fcn = @(v) 1/2 .* Car.Cl .* Car.A .* World.p .* v.^2; 
             
-            % Define driving forces
-            Car.N_fcn = @(v) (Car.W + Car.lift_fcn(v)./4);
-                        
+            % Misc definitions
+            Car.pajecka_fcn = @(N) World.grip_scale .* (Car.B4(1) - Car.B4(2).*N) .* N ...
+                                   .*heaviside(Car.B4(1) - Car.B4(2).*N)... % if < 0, = 0
+                                   .*heaviside(N); % if < 0, = 0
+            Car.L_fcn = @(v) Car.lift_fcn(v) .* 0.5 .* [Car.CoPb,Car.CoPb,Car.CoPa,Car.CoPa] ./ (Car.l) ...
+                         + Car.drag_fcn(v) .* Car.CoPh ./ (Car.l);
+            Car.trans_ratio = Car.primary_drive .* Car.final_drive .* Car.gear_ratio;
+            Car.shift_vel = Car.rpm(end) .* (2*pi/60) ./ Car.trans_ratio .* Car.wheel_radius;
+
+            % Calculate FxB,FxT,Fy
+            Car.axB_fcn = @(v) Car.approxAXB(v);
         end
-        function W = transferWeight(Car,ax,ay)
-            W = Car.m .* 9.8 ...
-                .* (... 
-                0.5 .* [Car.b,Car.b,Car.a,Car.a] ./ (Car.a+Car.b)... 
-                + ...
-                ay ./ [Car.tf,-Car.tf,Car.tr,-Car.tr] ...
-                .* (...
-                [Car.KF,Car.KF,Car.KR,Car.KR] .* (Car.h - mean([Car.zf,Car.zr])) ./ (Car.KF+Car.KR) ...
-                + ...
-                [Car.b,Car.b,Car.a,Car.a] .* [Car.zf,Car.zf,Car.zr,Car.zr] ./ (Car.a+Car.b) ...
-                ) + ...
-                [-1,-1,1,1] .* ax .* Car.h ./ (Car.a + Car.b));
-        end
-        function ax = findAX(Car,v)
-            % Hone in on ax
+        function ax = approxAXB(Car,v)
             ax = 0;
             err = 1;
-            W = Car.W;
             while abs(err) > 0.001
-                % Re-transfer wheel weights
-                W = Car.transferWeight(ax,0.0);
-
-                % Determine front and rear accelerations
-                ax_f = sum(Car.pajecka_fcn(W(1:2)) .* Car.ax_scale) ./ sum(W(1:2));
-                ax_r = sum(Car.pajecka_fcn(W(3:4)) .* Car.ax_scale) ./ sum(W(3:4));
-                
-                % Determine error in ax
+                Wt = Car.W + Car.m .* ([-1,-1,1,1] .* ax .* Car.h ./ (Car.l));
+                % (ax - Car.drag_fcn(v)./Car.m)
+                N = Wt - Car.L_fcn(v);
+                if any(N <= 0)
+                    axb_max = Car.l .* Car.b ./ Car.a .* -9.8;
+                    Wt = Car.W + Car.m .* ([-1,-1,1,1] .* axb_max .* Car.h ./ (Car.l));
+                    N = Wt - Car.L_fcn(v);
+                end
+                ax_f = sum(-Car.pajecka_fcn(N(1:2))) ./ sum(Wt(1:2) ./ 9.8);
+                ax_r = sum(-Car.pajecka_fcn(N(3:4))) ./ sum(Wt(3:4) ./ 9.8);
                 ax_temp = ax;
-                ax = min(ax_f,ax_r);
+                ax = max([ax_f,ax_r]);
                 err = ax - ax_temp;
+                disp([v,ax,ax_f,ax_r,err])
+                disp([N,any(N<=0)])
             end
         end
-        function ay = findAY(Car,v)
-            % Hone in on ay
-            ay = 0;
+        function ax = approxAXT(Car,v)
+            ax = 0;
             err = 1;
-            W = Car.W;
             while abs(err) > 0.001
-                W = transferWeight(Car,0,ay);
-                ay_temp = ay;
-                ay_f = sum(Car.pajecka_fcn(W(1:2))) ./ sum(W(1:2));
-                ay_r = sum(Car.pajecka_fcn(W(3:4))) ./ sum(W(3:4));
-                ay = min(ay_f,ay_r);
-                err = ay - ay_temp;
+                Wt = Car.W + Car.m .* ([-1,-1,1,1] .* ax .* Car.h ./ (Car.l));
+                N = Wt - Car.L_fcn(v);
+                ax_r = sum(+Car.pajecka_fcn(N(3:4))) ./ sum(Wt ./ 9.8);
+                ax_eng = Car.powertrainLookup(v) ./ Car.m;
+                ax_temp = ax;
+                ax = min([ax_r,ax_eng]);
+                err = ax - ax_temp;
+                % disp([ax,ax_r,ax_eng, err])
+                % disp(N)
+            end
+        end
+        function [Fx] = powertrainLookup(Car,v)
+            if v >= Car.shift_vel(end)
+                cur_gear = length(Car.gear_ratio);
+                rpm = Car.rpm(end);
+                Fx = Car.drag_fcn(v);
+            else
+                cur_gear = interp1([-1,Car.shift_vel],0:length(Car.gear_ratio),v,"next");
+                rpm = v ./ (Car.wheel_radius) .* (2*pi/60)^-1 .* Car.trans_ratio(cur_gear);
+                torque = interp1(Car.rpm,Car.torque,rpm,'linear','extrap') .* Car.trans_ratio(cur_gear);
+                Fx = torque ./ Car.wheel_radius;
             end
         end
     end
